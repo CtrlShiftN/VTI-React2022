207756e91c94bd5077e79c13537bc751e9b3c77e
Doctor <karuyanakaido24@gmail.com>
2022-08-02 14:20:07 +0700

    Signed-off-by: Doctor <karuyanakaido24@gmail.com>

--- Day 09 - Form/demo/src/index.js
+++ Day 09 - Form/demo/src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TodoCard from './components/TodoCard';
-import CardManager from './components/CardManager';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<CardManager />);
+root.render();
